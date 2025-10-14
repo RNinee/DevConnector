34e708f23d970ede4a3f431e5875a5217ee744fb
RNinee <phasin2006@gmail.com>
2025-10-14 08:43:59 +0700

    add config

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
@@ -9,4 +12,3 @@ const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
