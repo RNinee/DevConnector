6df76dc63de5424b98bee979e7231b5071a7d251
RNinee <phasin2006@gmail.com>
2025-10-15 14:32:41 +0700

    finishing up user with jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = 
+
 
 // @route    GET api/auth
 // @desc     Test route
 // @access   Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+    try{
+        const user = await 
+    } catch(err){
+
+    }
+});
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -34,7 +36,6 @@ router.post(
         return res.status(400).json([{ msg: 'User already exists' }]);
       }
 
-      // get users gravatar
       const avatar = gravatar.url(email, {
         s: '200',
         r: 'pg',
@@ -53,14 +54,28 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      // return jsonwebtoken
-      res.send('User registered');
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
     }
 
-    res.send('User registered');
+    //res.send('User registered');
   }
 );
 
