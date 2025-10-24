0cbc9b85c132fb793891452f7c50cf9e63b22fda
RNinee <phasin2006@gmail.com>
2025-10-24 13:36:59 +0700

    add profile browsing and GitHub repos integration

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -3,6 +3,8 @@ import {
   PROFILE_ERROR,
   CLEAR_PROFILE,
   UPDATE_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
 } from '../actions/type';
 
 const initialState = {
@@ -17,24 +19,36 @@ export default function (state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
-    case 'UPDATE_PROFILE':
-    case 'GET_PROFILE':
+    case UPDATE_PROFILE:
+    case GET_PROFILE:
       return {
         ...state,
         profile: payload,
         loading: false,
       };
-    case 'PROFILE_ERROR':
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false,
+      };
+    case PROFILE_ERROR:
       return {
         ...state,
         error: payload,
         loading: false,
       };
-    case 'CLEAR_PROFILE':
+    case CLEAR_PROFILE:
       return {
         ...state,
         profile: null,
         repos: [],
+        loading: false,
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false,
       };
     default:
